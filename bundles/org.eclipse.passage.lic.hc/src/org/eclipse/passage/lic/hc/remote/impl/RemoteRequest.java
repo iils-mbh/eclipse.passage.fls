4afe4d9cd23e586e25e0835d20c667d2ac3c23d9
Hannes Wellmann <hannes.wellmann@iils.de>
2026-09-07 17:28:55 +0200

    Support complete URIs as floating server IP
    This allows to specify different server URL schemes like https instead
    of just http or defining paths in the location of a FLS.

--- bundles/org.eclipse.passage.lic.hc/src/org/eclipse/passage/lic/hc/remote/impl/RemoteRequest.java
+++ bundles/org.eclipse.passage.lic.hc/src/org/eclipse/passage/lic/hc/remote/impl/RemoteRequest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020, 2021 ArSysOp
+ * Copyright (c) 2020, 2025 ArSysOp
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -9,6 +9,7 @@
  *
  * Contributors:
  *     ArSysOp - initial API and implementation
+ *     Hannes Wellmann (IILS mbh) - Support complete URIs as floating server IP
  *******************************************************************************/
 package org.eclipse.passage.lic.hc.remote.impl;
 
@@ -24,8 +25,9 @@ import org.eclipse.passage.lic.hc.remote.Connection;
 import org.eclipse.passage.lic.hc.remote.Request;
 import org.eclipse.passage.lic.hc.remote.RequestContext;
 import org.eclipse.passage.lic.internal.hc.i18n.AccessMessages;
-import org.eclipse.passage.lic.internal.net.HostPort;
 import org.eclipse.passage.lic.licenses.model.api.FloatingLicenseAccess;
+import org.eclipse.passage.lic.licenses.model.api.FloatingServerConnection;
+import org.eclipse.passage.lic.licenses.model.meta.LicensesPackage;
 
 /**
  * <p>
@@ -55,14 +57,21 @@ public abstract class RemoteRequest<C extends Connection> implements Request<C> 
 	@Override
 	public final URL url() throws LicensingException {
 		try {
-			HostPort corrdinates = new FloatingServerCoordinates(access).get();
-			return new URI("http", null, //$NON-NLS-1$
-					corrdinates.host(), //
-					Integer.parseInt(corrdinates.port()), null, //
-					parameters().query(), null).toURL();
-		} catch (LicensingException //
-				| NumberFormatException //
-				| MalformedURLException | URISyntaxException e) {
+			FloatingServerConnection server = access.getServer();
+			URI uri = new URI(server.getIp());
+			if (uri.getScheme() == null && uri.getAuthority() == null && uri.getPath() != null) {
+				// Server's IP is a pure host definition and should be used as such
+				uri = new URI("http", server.getIp(), null, null); //$NON-NLS-1$
+			}
+			int port = uri.getPort();
+			if (server.eIsSet(LicensesPackage.eINSTANCE.getFloatingServerConnection_Port())) {
+				port = server.getPort();
+			}
+			String query = parameters().query();
+			URI fullURI = new URI(uri.getScheme(), uri.getUserInfo(), uri.getHost(), port, //
+				uri.getPath(), query, uri.getFragment());
+			return fullURI.toURL();
+		} catch (LicensingException | MalformedURLException | URISyntaxException e) {
 			throw new LicensingException(AccessMessages.Request_failed_to_compose_url, e);
 		}
 	}
